f8e8d4ee519aa83f0fbbaf9e89d286025164685c
Ernie Hsiung <e@erniehsiung.com>
2021-08-06 10:27:44 -0400

    remove misleading comments

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -4,7 +4,5 @@ const router = express.Router();
 
 // Inventory Routes
 router.get("/inventories", inventoryController.sampleControllerMethod);
-// router.route("/inventories/:id").get(inventoryController.getAuthorById);
-// router.route("/inventories").post(inventoryController.newAuthor);
 
 module.exports = router;
